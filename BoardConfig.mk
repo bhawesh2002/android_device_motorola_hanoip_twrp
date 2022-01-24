55742e6e7e4bccbc2c211e44b02f09f6fe5b77a3
bhawesh2002 <bhaweshmankar2002@gmail.com>
2022-01-24 10:22:43 +0530

    modify boardconfig.mk to support kernel modules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,8 +56,6 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 BOARD_PROVIDES_GPTUTILS := true
 
 # Kernel
-BOARD_KERNEL_IMAGE_NAME := Image.gz
-
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
@@ -66,7 +64,6 @@ BOARD_KERNEL_CMDLINE += androidboot.memcg=1 androidboot.hardware=qcom androidboo
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 swiotlb=2048
-BOARD_KERNEL_CMDLINE += 
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hab.csv=5
@@ -84,17 +81,15 @@ BOARD_KERNEL_CMDLINE += androidboot.vbmeta.digest=94c13e24836a893accfd1a9883b9fe
 BOARD_KERNEL_CMDLINE += androidboot.veritymode=enforcing
 BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-
-BOARD_BOOT_HEADER_VERSION := 3
-BOARD_KERNEL_PAGESIZE := 4096
 
 BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_TAGS_OFFSET   := 0x00000100
-BOARD_KERNEL_OFFSET        := 0x00008000
-BOARD_RAMDISK_OFFSET       := 0x01000000
-BOARD_DTB_OFFSET           := 0x01f00000
+BOARD_KERNEL_PAGESIZE := 4096
+
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 
 #kernel source
 #TARGET_KERNEL_SOURCE := kernel/motorola/hanoip
@@ -102,108 +97,21 @@ BOARD_DTB_OFFSET           := 0x01f00000
 
 # prebuilts
 BOARD_PREBUILT_DTBIMAGE_DIR := $(PLATFORM_PATH)/prebuilt/dtb
-TARGET_PREBUILT_KERNEL := $(PLATFORM_PATH)/prebuilt/Image.gz
+TARGET_PREBUILT_KERNEL := $(PLATFORM_PATH)/prebuilt/kernel
 BOARD_PREBUILT_DTBOIMAGE := $(PLATFORM_PATH)/prebuilt/dtbo.img
-
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-
+BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/$(PLATFORM_PATH)/dtc/dtc
 
-#mkbootimg arguments
-BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --dtb $(BOARD_PREBUILT_DTBIMAGE_DIR)/dtb
-
 # kernel build info
-TARGET_KERNEL_VERSION := 4.14.190-perf
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
+#TARGET_KERNEL_VERSION := 4.14.190-perf
+#TARGET_KERNEL_ARCH := arm64
+#TARGET_KERNEL_HEADER_ARCH := arm64
 
 #clang
 #use if building with clang
 #TARGET_KERNEL_CLANG_COMPILE := true
 #TARGET_KERNEL_CLANG_VERSION := r383902b  # clang version for Android R release
-
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE  := 67108864
-BOARD_DTBOIMG_PARTITION_SIZE := 25165824
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 536870912
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 115921629184
-BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-
-#fstab
-TARGET_RECOVERY_FSTAB := device/motorola/hanoip/fstab.qcom
-
-# Workaround for error copying vendor files to recovery ramdisk
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-
-# persist.img
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
-BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# product.img
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_USES_PRODUCTIMAGE := true
-TARGET_COPY_OUT_PRODUCT := product
-
-# system_ext.img
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_SYSTEM_EXT := system_ext
-
-BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
-
-# Super
-BOARD_SUPER_PARTITION_SIZE := 10804527104
-BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
-#BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE is set to BOARD_SUPER_PARTITION_SIZE / 2 - 4MB
-BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE := 5398069248
-BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := \
-    system \
-    product \
-    vendor \
-    system_ext 
-
-# Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_INCLUDE_RECOVERY_DTBO := false
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-TARGET_USES_MKE2FS := true
-
-# A/B device flags
-AB_OTA_UPDATER := true
-TARGET_NO_KERNEL := false
-TARGET_NO_RECOVERY := true
-BOARD_USES_RECOVERY_AS_BOOT := true
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
-
-#vendor_lkm_dir := $(PLATFORM_PATH)/recovery/root/vendor/lib/modules
-#BOARD_RECOVERY_KERNEL_MODULES := \
-#  $(vendor_lkm_dir)/aw8646.ko \
-#  $(vendor_lkm_dir)/exfat.ko \
-#  $(vendor_lkm_dir)/fpc1020_mmi.ko \
-#  $(vendor_lkm_dir)/ilitek_v3_mmi.ko \
-#  $(vendor_lkm_dir)/mmi_annotate.ko \
-#  $(vendor_lkm_dir)/mmi_info.ko \
-#  $(vendor_lkm_dir)/mmi_sys_temp.ko \
-#  $(vendor_lkm_dir)/moto_f_usbnet.ko \
-#  $(vendor_lkm_dir)/sensors_class.ko \
-#  $(vendor_lkm_dir)/snd_smartpa_aw882xx.ko \
-#  $(vendor_lkm_dir)/utags.ko
 
 # AVB
 BOARD_AVB_ENABLE := true
@@ -227,23 +135,164 @@ BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
-# TWRP specific build flags
-#TW_LOAD_VENDOR_MODULES := "aw8697.ko ilitek_v3_mmi.ko"
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_INCLUDE_NTFS_3G := true
-TW_INCLUDE_RESETPROP := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_MAX_BRIGHTNESS := 400
-TW_DEFAULT_BRIGHTNESS := 225
-TW_THEME := portrait_hdpi
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
-TW_CUSTOM_BATTERY_PATH := "/sys/class/power_supply/qcom_battery"
-TW_NO_CPU_TEMP := true
-TW_NO_SCREEN_BLANK := true
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE  := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 536870912
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 115921629184
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+BOARD_RAMDISK_USE_LZ4 := true
+
+#fstab
+TARGET_RECOVERY_WIPE := device/motorola/hanoip/recovery.wipe
+TARGET_RECOVERY_FSTAB := device/motorola/hanoip/fstab.qcom
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# persist.img
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# product.img
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USES_PRODUCTIMAGE := true
+TARGET_COPY_OUT_PRODUCT := product
+
+# system_ext.img
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
+# Specify BOOT_KERNEL_MODULES
+#
+# modules for first stage in vendor_boot.img, remainder goes to vendor.img.
+# This list is a superset of all the possible modules required, and is
+# filtered and ordered as per the supplied kernel's modules.load file.
+# Any modules that do not exist will be silently dropped.  This is required
+# because some kernel configurations may have extra debug or test modules,
+# make sure any required to be loaded during first stage init are listed.
+
+BOOT_KERNEL_MODULES := \
+	audio_apr.ko \
+	audio_snd_event.ko \
+	audio_wglink.ko \
+	audio_q6_pdr.ko \
+	audio_q6_notifier.ko \
+	audio_adsp_loader.ko \
+	audio_q6.ko \
+	audio_usf.ko \
+	audio_pinctrl_wcd.ko \
+	audio_pinctrl_lpi.ko \
+	audio_swr.ko \
+	audio_wcd_core.ko \
+	audio_swr_ctrl.ko \
+	audio_wsa881x.ko \
+	audio_platform.ko \
+	audio_hdmi.ko \
+	audio_stub.ko \
+	audio_wcd9xxx.ko \
+	audio_mbhc.ko \
+	audio_wcd934x.ko \
+	audio_wcd937x.ko \
+	audio_wcd937x_slave.ko \
+	audio_bolero_cdc.ko \
+	audio_wsa_macro.ko \
+	audio_va_macro.ko \
+	audio_rx_macro.ko \
+	audio_tx_macro.ko \
+	audio_wcd_spi.ko \
+	audio_native.ko \
+	audio_machine_talos.ko \
+	mpq-adapter.ko \
+	mpq-dmx-hw-plugin.ko \
+	qca_cld3_wlan.ko \
+	rdbg.ko \
+	exfat.ko \
+	mmi_sys_temp.ko \
+	sensors_class.ko \
+	utags.ko \
+	moto_f_usbnet.ko \
+	watchdogtest.ko \
+	mmi_info.ko \
+	mmi_annotate.ko \
+	tzlog_dump.ko \
+	watchdog_cpu_ctx.ko \
+	fpsensor_spi_tee.ko \
+	fpc1020_mmi.ko \
+	ilitek_v3_mmi.ko \
+	sx933x_sar.ko \
+	snd_smartpa_aw882xx.ko \
+	qpnp_adaptive_charge.ko 
+	
+#symlink folders
+BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
+BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
+
+# Super
+BOARD_SUPER_PARTITION_SIZE := 10804527104
+BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
+#BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE is set to BOARD_SUPER_PARTITION_SIZE / 2 - 4MB
+BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE := 5398069248
+BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    system \
+    product \
+    vendor \
+    system_ext 
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_INCLUDE_RECOVERY_DTBO := false
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+TARGET_USES_MKE2FS := true
+
+# A/B device flags
+AB_OTA_UPDATER := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+
+# Kernel modules
+KERNEL_MODULE_DIR := $(PLATFORM_PATH)/recovery/root/vendor/lib/modules
+KERNEL_MODULES := $(wildcard $(KERNEL_MODULE_DIR)/*.ko)
+KERNEL_MODULES_LOAD := $(KERNEL_MODULE_DIR)/modules.load
+BOARD_VENDOR_KERNEL_MODULES := $(KERNEL_MODULES)
+OARD_VENDOR_KERNEL_MODULES_LOAD := $(KERNEL_MODULES_LOAD)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(KERNEL_MODULES)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(KERNEL_MODULES_LOAD)
+RECOVERY_KERNEL_MODULES := $(BOOT_KERNEL_MODULES)
+
+# Encryption
+PLATFORM_VERSION := 16.1.0
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+BOARD_USES_METADATA_PARTITION := true
+TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
+
+# Extras
+TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
+
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+
+TARGET_USES_LOGD := true
+
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Debug flags
+TWRP_INCLUDE_LOGCAT := true
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hidl.base@1.0 \
@@ -271,32 +320,29 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libprocinfo.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
 
-# Encryption
-PLATFORM_VERSION := 16.1.0
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
+
+# TWRP specific build flags
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_RESETPROP := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_MAX_BRIGHTNESS := 400
+TW_DEFAULT_BRIGHTNESS := 225
 TW_INCLUDE_CRYPTO := true
-BOARD_USES_QCOM_FBE_DECRYPTION := true
-BOARD_USES_METADATA_PARTITION := true
-TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
-
-# Extras
-TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
-
-BOARD_SUPPRESS_SECURE_ERASE := true
+TW_THEME := portrait_hdpi
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
+TW_CUSTOM_BATTERY_PATH := "/sys/class/power_supply/qcom_battery"
+TW_NO_CPU_TEMP := true
+TW_NO_SCREEN_BLANK := true
 TW_USE_TOOLBOX := true
 TW_HAS_EDL_MODE := true
-
-# Asian region languages
 TW_EXTRA_LANGUAGES := true
-# TW_DEFAULT_LANGUAGE := zh_CN
-
 # Notch Offset
 TW_Y_OFFSET := 120
 TW_H_OFFSET := -120
-
-# Debug flags
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-
-ALLOW_MISSING_DEPENDENCIES := true
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
+# TW_DEFAULT_LANGUAGE := zh_CN
