468e3313002519c847028490d0c8af0b60a75d5b
bhawesh2002 <bhaweshmankar2002@gmail.com>
2022-02-05 22:06:32 +0530

    a few changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,8 @@ BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery/root/system/etc/recovery.fstab
+
 # Workaround for error copying vendor files to recovery ramdisk
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
